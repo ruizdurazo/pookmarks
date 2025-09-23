5b9c4a19b86dfc21cd2b63ed86af4ca79fe1ba84
Enrique Ruiz Durazo <enrique.ruizdurazo@gmail.com>
2025-09-23 14:49:40 +0200

    Add keyboard shortcut

--- src/background.ts
+++ src/background.ts
@@ -3,3 +3,13 @@ chrome.runtime.onInstalled.addListener(() => {
     .setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error))
 })
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'open-pookmarks') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]?.id) {
+        chrome.sidePanel.open({ tabId: tabs[0].id });
+      }
+    });
+  }
+});
